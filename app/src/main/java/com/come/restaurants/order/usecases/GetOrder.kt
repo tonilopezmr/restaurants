151788a760d0797ebb3439c09be81147b339c9c3
Antonio López Marín <tonilopezmr@gmail.com>
2016-11-28 18:52:27 +0100

    Refactor OrderRepository to interface and added stub

--- app/src/main/java/com/come/restaurants/order/usecases/GetOrder.kt
+++ app/src/main/java/com/come/restaurants/order/usecases/GetOrder.kt
@@ -1,9 +1,9 @@
 package com.come.restaurants.order.usecases
 
 import com.come.restaurants.order.Order
-import com.come.restaurants.order.repository.OrderRepository
+import com.come.restaurants.order.persistence.network.FirebaseOrderRepository
 
-class GetOrder(val repository: OrderRepository) {
+class GetOrder(val repository: FirebaseOrderRepository) {
 
     fun get(id: String): Order {
         return repository.getOrder(id)
