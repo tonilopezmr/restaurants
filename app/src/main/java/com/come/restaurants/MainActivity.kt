1eb4a373ada814234c3afdb5fbfce4d21009dd4c
Cotel <miguelcoletomunoz@hotmail.com>
2016-11-28 17:55:48 +0100

    ListView working

--- app/src/main/java/com/come/restaurants/MainActivity.kt
+++ app/src/main/java/com/come/restaurants/MainActivity.kt
@@ -2,11 +2,34 @@ package com.come.restaurants
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.RelativeLayout
+import com.come.restaurants.order.Order
+import com.come.restaurants.order.OrderLine
+import com.come.restaurants.order.Plate
+import com.come.restaurants.ui.list.adapter.ListViewAdapter
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list_view)
+
+        val orderList = ArrayList<Order>()
+        var orderLines = ArrayList<OrderLine>()
+        val examplePlate = Plate("Plato", "Chivito", 5.0, emptyList())
+
+        orderLines.add(OrderLine("Linea", 2, examplePlate))
+        orderList.add(Order("Bla", "Bla bla", Date().time, orderLines))
+
+        val orderRecyclerView = findViewById(R.id.recyclerView) as RecyclerView
+        val adapter = ListViewAdapter(orderList)
+        orderRecyclerView.layoutManager = LinearLayoutManager(this)
+        orderRecyclerView.adapter = adapter
     }
 }
