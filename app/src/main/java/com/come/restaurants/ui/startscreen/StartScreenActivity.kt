0137781bc3e10c61c2a91df17284bab9cbe8d2c3
Kevin Zarzycki Szymanski <kezarszy@inf.upv.es>
2016-11-30 20:50:02 +0100

    created StartScreen layout

---
+++ app/src/main/java/com/come/restaurants/ui/startscreen/StartScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.come.restaurants.ui.startscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.come.restaurants.R
+
+class StartScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_screen)
+    }
+}
