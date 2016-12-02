f639f2a4dbff69f057d5b64ba2640f8cd8555913
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2016-12-02 13:11:28 +0100

    Starting firebase structure

--- app/src/main/java/com/come/restaurants/order/persistence/network/FirebaseOrderRepository.kt
+++ app/src/main/java/com/come/restaurants/order/persistence/network/FirebaseOrderRepository.kt
@@ -3,15 +3,15 @@ package com.come.restaurants.order.persistence.network
 import com.come.restaurants.order.domain.model.Order
 import com.come.restaurants.order.domain.OrderRepository
 import com.come.restaurants.order.domain.usecases.*
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import java.util.*
 
 class FirebaseOrderRepository : OrderRepository {
     private var database : FirebaseDatabase
     private var reference : DatabaseReference
     init {
         database = FirebaseDatabase.getInstance()
-        reference = database.getReference("")
+        reference = database.getReference("restaurant/vella")
     }
 
     override fun getOrder(id: String, callback: GetOrder.Callback) {
@@ -27,7 +27,17 @@ class FirebaseOrderRepository : OrderRepository {
     }
 
     override fun getOrders(callback: GetOrders.Callback) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+       reference.child("orders").addListenerForSingleValueEvent(object : ValueEventListener{
+           override fun onCancelled(databaseError: DatabaseError){
+               callback.error(Exception(databaseError.message))
+           }
+
+           override fun onDataChange(dataSnapshot: DataSnapshot){
+               val orders = dataSnapshot.children.map { it.getValue(Order ::class.java) }
+               callback.ordersReceived(orders)
+           }
+
+       })
     }
 
 }
