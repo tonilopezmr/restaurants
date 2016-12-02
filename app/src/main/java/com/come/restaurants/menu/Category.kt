b83846073c97d8317c466ca0755723f008e8a528
Antonio López Marín <tonilopezmr@gmail.com>
2016-12-02 21:00:57 +0100

    Changed GroupMenu to Category

--- app/src/main/java/com/come/restaurants/menu/Category.kt
+++ app/src/main/java/com/come/restaurants/menu/Category.kt
@@ -2,4 +2,4 @@ package com.come.restaurants.menu
 
 import com.come.restaurants.order.domain.model.Plate
 
-class MenuGroup(val name: String, val plates: List<Plate>)
+class Category(val name: String, val plates: List<Plate>)
--- app/src/main/java/com/come/restaurants/menu/Menu.kt
+++ app/src/main/java/com/come/restaurants/menu/Menu.kt
@@ -1,3 +1,3 @@
 package com.come.restaurants.menu
 
-class Menu(val groups: List<MenuGroup>)
+class Menu(val groups: List<Category>)
