a0d526c7d00bd3a39762d76ce20c77191d3f089d
Cotel <miguelcoletomunoz@hotmail.com>
2016-12-03 21:10:56 +0100

    Added empty constructors to models

--- app/src/main/java/com/come/restaurants/order/domain/model/Order.kt
+++ app/src/main/java/com/come/restaurants/order/domain/model/Order.kt
@@ -1,9 +1,9 @@
 package com.come.restaurants.order.domain.model
 
-class Order(val id: String,
-            val code: String,
-            val timestamp: Long,
-            val orderLines: List<OrderLine>) {
+class Order(val id: String = "",
+            val code: String = "",
+            val timestamp: Long = 0,
+            val orderLines: List<OrderLine> = emptyList()) {
 
     fun getPrice(): Double {
         return orderLines.fold(0.0) { price, orderLine ->
