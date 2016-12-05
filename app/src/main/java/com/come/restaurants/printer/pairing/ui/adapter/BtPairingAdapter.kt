8d03b4dfa3b958009e347d337d0129bda91bf220
Cotel <miguelcoletomunoz@hotmail.com>
2016-12-05 10:15:19 +0100

    Pairing devices correctly now

--- app/src/main/java/com/come/restaurants/printer/pairing/ui/adapter/BtPairingAdapter.kt
+++ app/src/main/java/com/come/restaurants/printer/pairing/ui/adapter/BtPairingAdapter.kt
@@ -2,6 +2,7 @@ package com.come.restaurants.printer.pairing.ui.adapter
 
 import android.bluetooth.BluetoothDevice
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,8 @@ import java.util.*
 
 
 class BtPairingAdapter() : RecyclerView.Adapter<BtPairingAdapter.ListViewHolder>() {
+
+    private val TAG = "BtPairing"
 
     val printerList: MutableList<BluetoothDevice> = ArrayList()
 
@@ -29,8 +32,16 @@ class BtPairingAdapter() : RecyclerView.Adapter<BtPairingAdapter.ListViewHolder>
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         holder.bindPrinter(printerList[position])
         holder.itemView.setOnClickListener { it ->
-            val toast = Toast.makeText(it.context, printerList[position].name, Toast.LENGTH_LONG)
-            toast.show()
+            val device = printerList[position]
+            if (device.bondState == BluetoothDevice.BOND_BONDED) {
+                val method = device.javaClass.getMethod("removeBond")
+                val result = method.invoke(device)
+                Log.d(TAG, "Device was unpaired correctly: $result")
+            } else {
+                val method = device.javaClass.getMethod("createBond")
+                val result = method.invoke(device)
+                Log.d(TAG, "Device was paired correctly: $result")
+            }
         }
     }
 
