9291a67fd0c9768057b551433dbd58b9224345ee
Antonio López Marín <tonilopezmr@gmail.com>
2016-12-07 00:30:36 +0100

    Fix the order sorted list

--- app/src/androidTest/java/com/come/restaurants/order/list/ui/OrderListActivityShould.kt
+++ app/src/androidTest/java/com/come/restaurants/order/list/ui/OrderListActivityShould.kt
@@ -6,12 +6,12 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.RecyclerViewActions
-import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.core.deps.guava.collect.Ordering
 import android.support.test.espresso.intent.Intents.intended
 import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import android.support.test.espresso.intent.rule.IntentsTestRule
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.filters.LargeTest
-import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.util.SortedList
 import android.support.v7.widget.RecyclerView
@@ -22,6 +22,8 @@ import com.come.restaurants.order.list.ui.adapter.OrderListAdapter
 import com.come.restaurants.order.list.ui.matchers.RecyclerViewItemsCountMatcher
 import com.come.restaurants.order.list.ui.viewassertion.RecyclerSortedViewAssertion
 import com.come.restaurants.order.persistence.stubs.StubOrderRepository
+import org.hamcrest.StringDescription
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,8 +33,8 @@ import org.junit.runner.RunWith
 class OrderListActivityShould {
 
     @Rule
-    fun orderListActivityTestRule(): ActivityTestRule<OrderListActivity> =
-            ActivityTestRule<OrderListActivity>(OrderListActivity::class.java)
+    fun orderListActivityTestRule(): IntentsTestRule<OrderListActivity> =
+        IntentsTestRule<OrderListActivity>(OrderListActivity::class.java)
 
     @Test
     fun show_all_characters_size_in_list_view() {
@@ -46,11 +48,19 @@ class OrderListActivityShould {
     @Test
     fun recycler_view_should_be_sorted_by_timestamp() {
         val withAdapter = object : RecyclerSortedViewAssertion.WithAdapter<Long> {
+
+            override fun assert(description: StringDescription, list: List<Long>) {
+                assertTrue(description.toString(),
+                    Ordering.natural<Long>()
+                            .reverse<Long>()
+                            .isOrdered(list))
+            }
+
             override fun itemsToSort(recyclerView: RecyclerView): List<Long> {
                 val adapter = recyclerView.adapter as OrderListAdapter
                 var items: SortedList<Order> = adapter.getItems()
 
-                return (0..items.size()).map { items[it].timestamp }
+                return (0 until items.size()).map { items[it].timestamp }
             }
         }
 
@@ -60,12 +70,10 @@ class OrderListActivityShould {
 
     @Test
     fun item_click_should_start_details_activity() {
-        Intents.init()
         onView(withId(R.id.ordersRecyclerView))
                 .perform(RecyclerViewActions
                         .actionOnItemAtPosition<OrderListAdapter.ListViewHolder>(1, click()))
 
         intended(hasComponent(ComponentName(getTargetContext(), OrderDetailActivity::class.java)))
-        Intents.release()
     }
 }
