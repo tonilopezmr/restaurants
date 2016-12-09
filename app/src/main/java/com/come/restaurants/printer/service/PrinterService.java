180801a557d100ab72f1ab3b9783d4ac33f065a6
Antonio López Marín <tonilopezmr@gmail.com>
2016-12-09 21:08:31 +0100

    Removed unnecesary try catch

--- app/src/main/java/com/come/restaurants/printer/service/PrinterService.java
+++ app/src/main/java/com/come/restaurants/printer/service/PrinterService.java
@@ -17,38 +17,37 @@ public class PrinterService {
   }
 
   /**
-     * Method that initializes the printer to start a Job Session
-     */
+   * Method that initializes the printer to start a Job Session
+   */
   public void initializePrinter() throws PrinterException {
-      printer.initialize();
-      config = new PrintConfig();
+    printer.initialize();
+    config = new PrintConfig();
   }
 
   /**
-     * Method that prints the arg line using the PrinterJob and then resets the config
-     * By default it prints a plain line
-     *
-     * @param line
-     * @throws PrinterException
-     */
+   * Method that prints the arg line using the PrinterJob and then resets the config
+   * By default it prints a plain line
+   *
+   * @param line
+   * @throws PrinterException
+   */
   public void printLine(String line) throws PrinterException {
     try {
       printer.alignment(config.getAlignment());
       printer.font(config.getFont());
       printer.write(line + PrinterCommands.NEW_LINE);
     } catch (PrinterException e) {
+      initializePrinter();
       throw e;
     }
-
-    initializePrinter();
   }
 
   /**
-     * Prints all whiteLines passed with the same PrinterJob config and then resets the config
-     *
-     * @param lines
-     * @throws PrinterException
-     */
+   * Prints all whiteLines passed with the same PrinterJob config and then resets the config
+   *
+   * @param lines
+   * @throws PrinterException
+   */
   public void printLines(List<String> lines) throws PrinterException {
     try {
       printer.alignment(config.getAlignment());
@@ -57,104 +56,95 @@ public class PrinterService {
         printer.write(line + PrinterCommands.NEW_LINE);
       }
     } catch (PrinterException e) {
+      initializePrinter();
       throw e;
     }
-
-    initializePrinter();
   }
 
   /**
-     * Method that prints a separator.
-     * Default: ----------------
-     *
-     * @throws PrinterException
-     */
+   * Method that prints a separator.
+   * Default: ----------------
+   *
+   * @throws PrinterException
+   */
   public PrinterService printSeparator() throws PrinterException {
-    try {
-      printer.write(config.getSeparator() + config.getWhiteLines());
-    } catch (PrinterException e) {
-      throw e;
-    }
+    printer.write(config.getSeparator() + config.getWhiteLines());
     return this;
   }
 
   /**
-     * Allows to specify the separator to be used in the print
-     *
-     * @param separator
-     * @return
-     */
+   * Allows to specify the separator to be used in the print
+   *
+   * @param separator
+   * @return
+   */
   public PrinterService setSeparator(String separator) {
     config.setSeparator(separator);
     return this;
   }
 
   /**
-     * Method that sets the number of newLines that goes after a separator
-     * Default: 2 new whiteLines
-     *
-     * @param lines
-     */
+   * Method that sets the number of newLines that goes after a separator
+   * Default: 2 new whiteLines
+   *
+   * @param lines
+   */
   public PrinterService whiteLines(int lines) {
     config.setWhiteLines(lines);
     return this;
   }
 
   /**
-     * Changes the alignment in the print
-     *
-     * @param align
-     * @return
-     */
+   * Changes the alignment in the print
+   *
+   * @param align
+   * @return
+   */
   public PrinterService alignment(PrinterCommands.Align align) {
     config.setAlignment(align);
     return this;
   }
 
   /**
-     * Changes the font to be used in the print
-     *
-     * @param font
-     * @return
-     */
+   * Changes the font to be used in the print
+   *
+   * @param font
+   * @return
+   */
   public PrinterService font(PrinterCommands.Font font) {
     config.setFont(font);
     return this;
   }
 
   /**
-     * Feeds paper
-     *
-     * @param feed
-     * @return
-     * @throws PrinterException
-     */
+   * Feeds paper
+   *
+   * @param feed
+   * @return
+   * @throws PrinterException
+   */
   public PrinterService feed(PrinterCommands.FeedPaper feed) throws PrinterException {
-    try {
-      printer.feed(feed);
-    } catch (PrinterException e) {
-      throw new PrinterException(e);
-    }
+    printer.feed(feed);
     return this;
   }
 
   /**
-     * Sets the parameter config to be used on the print
-     *
-     * @param config
-     * @return
-     */
+   * Sets the parameter config to be used on the print
+   *
+   * @param config
+   * @return
+   */
   public PrinterService withConfig(PrintConfig config) {
     this.config = config;
     return this;
   }
 
   /**
-     * Retrieves the current configuration to be used on the print
-     * WARNING: After a print, it returns the default configuration
-     *
-     * @return
-     */
+   * Retrieves the current configuration to be used on the print
+   * WARNING: After a print, it returns the default configuration
+   *
+   * @return
+   */
   public PrintConfig getConfig() {
     return this.config;
   }
