592ab1e8b1c334f2d82d3287aebf2b7af047d9c1
Cotel <miguelcoletomunoz@hotmail.com>
2016-12-10 13:11:01 +0100

    Adding stub restaurant for login

--- app/src/main/java/com/come/restaurants/restaurant/persistence/stubs/StubRestaurantRepository.kt
+++ app/src/main/java/com/come/restaurants/restaurant/persistence/stubs/StubRestaurantRepository.kt
@@ -27,15 +27,19 @@ class StubRestaurantRepository : RestaurantRepository {
 
     restaurants.add(Restaurant(
         "La Vella",
-        "7e79497526e08d1e87ee778364101f27ba062e77d8d21bda6956ff47c8c1a9fe",
+        "7e79497526e08d1e87ee778364101f27ba062e77d8d21bda6956ff47c8c1a9fe", // lavella
         "1",
         menu))
     restaurants.add(Restaurant(
         "Conservatorio",
-        "1c4a5a3f18a6f390ffab9aa5d5585bf80ff31b3a6c0fe1f1761d5da881f66c4c",
+        "1c4a5a3f18a6f390ffab9aa5d5585bf80ff31b3a6c0fe1f1761d5da881f66c4c", // conservatorio
         "2",
-        menu
-    ))
+        menu))
+    restaurants.add(Restaurant(
+        "test",
+        "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4", // 1234
+        "1",
+        menu))
 
   }
 }
