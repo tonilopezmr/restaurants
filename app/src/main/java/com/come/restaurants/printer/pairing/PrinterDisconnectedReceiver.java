b6131c8bc34e4b2978bb5d41a11157317e019ecd
Adriano Vega <adrianovega@hotmail.com>
2017-02-17 10:39:14 +0100

    Added AlertDialog that appears after printer has disconnected (... again)

--- app/src/main/java/com/come/restaurants/printer/pairing/PrinterDisconnectedReceiver.java
+++ app/src/main/java/com/come/restaurants/printer/pairing/PrinterDisconnectedReceiver.java
@@ -1,11 +1,12 @@
 package com.come.restaurants.printer.pairing;
 
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.hardware.usb.UsbManager;
-import android.widget.Toast;
 
 import com.come.restaurants.R;
 
@@ -19,18 +20,29 @@ public class PrinterDisconnectedReceiver extends BroadcastReceiver {
       String action = intent.getAction();
       if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED) ||
           action.equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
-        launchChoosingDevice(context, intent, context.getString(R.string.error_printer_disconnected));
+        launchChoosePairingDevice(context, intent);
       }
     }
   }
 
-  private void launchChoosingDevice(Context context, Intent intent, String toastMessage) {
-    Toast.makeText(context, toastMessage, Toast.LENGTH_LONG)
-        .show();
-    Intent startPrinterPairing = new Intent(context, ChoosePairingActivity.class);
-    //WARNING!!! TO REVIEW
-    startPrinterPairing.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    context.startActivity(startPrinterPairing);
+  private void launchChoosePairingDevice(final Context context, Intent intent) {
+    new AlertDialog.Builder(context)
+        .setTitle(R.string.error_printer_disconnected)
+        .setMessage(R.string.prompt_reconnect_printer)
+        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            Intent startPrinterPairing = new Intent(context, ChoosePairingActivity.class);
+            startPrinterPairing.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(startPrinterPairing);
+          }
+        })
+        .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            //do nothing
+          }
+        }).show();
   }
   public static boolean hasPrinterBeenPaired() {
     return hasPrinterBeenPaired;
