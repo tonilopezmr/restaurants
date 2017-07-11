7fc221f49309b873f53bf4b01b9ee2162b79c15b
Antonio López Marín <tonilopezmr@gmail.com>
2017-07-11 20:24:17 +0200

    Fix to lateinit references

--- app/src/main/java/com/come/restaurants/DI/DependencyInjector.kt
+++ app/src/main/java/com/come/restaurants/DI/DependencyInjector.kt
@@ -2,6 +2,7 @@ package com.come.restaurants.DI
 
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import com.come.restaurants.order.domain.OrderRepository
 import com.come.restaurants.order.domain.usecases.GetNewOrder
 import com.come.restaurants.order.domain.usecases.GetOrder
 import com.come.restaurants.order.domain.usecases.GetOrders
@@ -9,9 +10,11 @@ import com.come.restaurants.order.domain.usecases.PrintOrder
 import com.come.restaurants.order.persistence.network.FirebaseOrderRepository
 import com.come.restaurants.printer.domain.PrinterRepository
 import com.come.restaurants.printer.domain.usecases.PrintWelcome
+import com.come.restaurants.printer.service.Printer
 import com.come.restaurants.printer.service.PrinterFactory
 import com.come.restaurants.printer.service.PrinterQueue
 import com.come.restaurants.printer.service.PrinterService
+import com.come.restaurants.restaurant.domain.RestaurantRepository
 import com.come.restaurants.restaurant.domain.usecases.Close
 import com.come.restaurants.restaurant.domain.usecases.Open
 import com.come.restaurants.restaurant.persistence.network.FirebaseRestaurantRepository
@@ -19,15 +22,21 @@ import com.come.restaurants.restaurant.persistence.network.FirebaseRestaurantRep
 object DependencyInjector {
 
   lateinit var context: Context
-  val restaurantRepository = FirebaseRestaurantRepository(context.getSharedPreferences("shared", MODE_PRIVATE))
-  val orderRepository = FirebaseOrderRepository(context.getSharedPreferences("shared", MODE_PRIVATE))
-  val printer = PrinterFactory.getPrinter()
-  val printerJob = PrinterService(printer)
-  var printerRepository = PrinterRepository(printerJob)
-  val printerQueue = PrinterQueue(printerRepository)
+  lateinit var restaurantRepository: RestaurantRepository
+  lateinit var orderRepository: OrderRepository
+  lateinit var printer: Printer
+  lateinit var printerJob: PrinterService
+  lateinit var printerRepository: PrinterRepository
+  lateinit var printerQueue: PrinterQueue
 
   fun init(context: Context) {
     this.context = context
+    restaurantRepository = FirebaseRestaurantRepository(DependencyInjector.context.getSharedPreferences("shared", MODE_PRIVATE))
+    orderRepository = FirebaseOrderRepository(DependencyInjector.context.getSharedPreferences("shared", MODE_PRIVATE))
+    printer = PrinterFactory.getPrinter()
+    printerJob = PrinterService(printer)
+    printerRepository = PrinterRepository(printerJob)
+    printerQueue = PrinterQueue(printerRepository)
   }
 
   fun getOrder(): GetOrder {
